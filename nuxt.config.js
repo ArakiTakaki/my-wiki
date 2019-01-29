63e3733f8efb27fcba2a2f37370e7acec809e82c
ArakiTakaki <araki_takaki@yahoo.co.jp>
2019-01-29 16:27:03 +0900

    initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,78 @@
+const pkg = require('./package')
+
+
+module.exports = {
+  /**
+   * spa | universal
+  */
+  mode: 'spa',
+
+  /**
+   * ソースの存在しているディレクトリ
+   */
+  srcDir: 'app',
+
+  /**
+   * Vue Routerの設定を上書きできる
+   */
+  router: {
+    /**
+     * https://ja.nuxtjs.org/api/configuration-router
+     * mode : ルーティングのモードを設定します。サーバーサイドレンダリングのため、この設定を変更することは非推奨です。
+    */
+    mode: 'hash'
+  },
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+  ],
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+      /**
+       * webpackの出力先
+       */
+      config.output.publicPath = './_nuxt/'
+    }
+  }
+}
