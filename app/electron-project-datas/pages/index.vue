4570ed19e87f9aff5ec947d8086634d0cdfd47a5
ArakiTakaki <araki_takaki@yahoo.co.jp>
2019-01-29 22:34:26 +0900

    emitterを試してみた

--- app/electron-project-datas/pages/index.vue
+++ app/electron-project-datas/pages/index.vue
@@ -1,24 +1,34 @@
 <template lang="pug">
   .root
     h1 sample
+    button(@click="onClick") test
 </template>
 
 <script>
-import Vue from 'vue';
-import { Component, Prop } from 'vue-property-decorator';
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
 // Components
 // import Input from '../components/Input.vue';
 @Component({
   // components: {Input}
 })
 export default class Index extends Vue {
-  count = 3
-  sample = 'test'
-  countUp () {
-    this.count++
+  count = 3;
+  sample = "test";
+  countUp() {
+    this.count++;
   }
   input(value) {
-    console.log(value)
+    console.log(value);
+  }
+  created() {
+    console.log(this);
+    this.$eventEmitter.on("sample", value => {
+      console.log(value);
+    });
+  }
+  onClick() {
+    this.$eventEmitter.emit("sample", { a: "b" });
   }
 }
 </script>
